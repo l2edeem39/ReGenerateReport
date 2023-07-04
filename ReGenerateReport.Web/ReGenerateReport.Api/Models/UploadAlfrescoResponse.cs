4164d6be9e4c124d7bb28a9e7f03a1d2aef98b3b
prodigy59 <prodigy59@viriyah.co.th>
2023-07-04 11:51:13 +0700

    [Edit] : add Model

---
+++ ReGenerateReport.Web/ReGenerateReport.Api/Models/UploadAlfrescoResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReGenerateReport.Api.Models
+{
+    public class UploadAlfrescoResponse
+    {
+        public string statusCode { get; set; }
+        public string message { get; set; }
+
+    }
+}
