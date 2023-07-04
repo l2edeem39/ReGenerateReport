11bbf8be674e7ac9b6605b2a76654d7011fd8ae8
prodigy59 <prodigy59@viriyah.co.th>
2023-07-04 13:46:33 +0700

    [Edit] : add function upload PDF

---
+++ ReGenerateReport.Web/Models/RequestUploadPDF.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReGenerateReport.Web.Models
+{
+    public class RequestUploadPDF
+    {
+        public string policyNo { get; set; }
+        public string templateId { get; set; }
+    }
+}
---
+++ ReGenerateReport.Web/Models/ResponseUploadPDF.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReGenerateReport.Web.Models
+{
+    public class ResponseUploadPDF
+    {
+        public string statusCode { get; set; }
+        public string message { get; set; }
+    }
+}
